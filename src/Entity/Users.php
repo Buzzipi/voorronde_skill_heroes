62747b204e4297df604b35649737329500c02a4d
Richard <143696669+Buzzipi@users.noreply.github.com>
2025-10-06 11:51:21 +0200

    edited Users entity
    Made sure to edit the users entity to make sure it's compatible with symfony's auth
    
    the following needed to be added:
    
    - Get and Setter for password
    - getUserIdentifiier()
    - eraseCredentials()
    
    and the entity needed to implement the UserInterface dependency

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -4,9 +4,16 @@ namespace App\Entity;
 
 use App\Repository\UsersRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\DBAL\Types\Types;
+
+//for password encryption
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+//to implement user login 
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UsersRepository::class)]
-class Users
+class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -15,6 +22,14 @@ class Users
 
     #[ORM\Column(length: 200)]
     private ?string $Name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Email = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Password = null;
+
+    private $roles = [];
 
     public function getId(): ?int
     {
@@ -31,5 +46,50 @@ class Users
         $this->Name = $Name;
 
         return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): static
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->Password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->Password = $password;
+
+        return $this;
+    }
+
+        
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function eraseCredentials(): void
+    {
+        // If you store temporary sensitive data, clear it here
     }
 }
